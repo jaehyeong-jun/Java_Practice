29038370d1170b1ce9342b2f19d563d2000c7c80
jaehyeong-jun <wjswogud0229@gmail.com>
2025-11-28 15:18:58 +0900

    Add files via upload

---
+++ Study/ArrayCreateByValueListEx/src/Example2.java
@@ -0,0 +1,12 @@
+public class Example2 {
+    public static void main(String[] args) {
+
+        int[] scores;
+        scores = new int[]{11,12,31};
+
+        for(int i = 0; i < scores.length; i++)
+        {
+            System.out.println(scores[i]);
+        }
+    }
+}
